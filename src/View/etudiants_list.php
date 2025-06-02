cd2421c180e545bc0b8f0cc58ec7360be30431d3
Yana-Shrom <dertilemene15@gmail.com>
2025-06-02 12:27:10 +0200

    cleaning

--- src/View/etudiants_list.php
+++
@@ -1,6 +0,0 @@
-<h1>Liste des étudiants</h1>
-<ul>
-<?php foreach ($etudiants as $etudiant): ?>
-    <li><?= $etudiant->getINE() ."-".  htmlspecialchars($etudiant->getNom()) ?> (<?= htmlspecialchars($etudiant->getEmail()) ?>)</li>
-<?php endforeach; ?>
-</ul>
